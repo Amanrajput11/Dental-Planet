e246199af5ab09db46bde90f4f2729660ee6566f
vaishalis04 <vaishalisingh0482@gmail.com>
2026-02-06 13:16:50 +0530

    changes by vaishali

--- app/doctors/dr-ankur/page.tsx
+++ app/doctors/dr-ankur/page.tsx
@@ -112,7 +112,7 @@ export default function DrAnkurPage() {
                 <br />
                 Tilak Nagar, Indore
               </p>
-              <p className="mb-1">📞 9893523334, 7400630334</p>
+              <p className="mb-1">📞 9301665066, 7400630334</p>
               <p className="mb-0">✉️ kbdentalplanet@gmail.com</p>
             </div>
           </div>
